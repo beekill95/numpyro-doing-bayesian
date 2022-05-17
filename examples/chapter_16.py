e7ca8ef9a69868fceb5bc913053ecfdd4c42c97f
beekill95 <nguyenmbquan95@gmail.com>
2022-05-17 10:08:23 -0400

    Add implementation for 1 group model in chapter 16.

---
+++ examples/chapter_16.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: doing_bayes
+#     language: python
+#     name: doing_bayes
+# ---
+
+# %cd ../..
+# %load_ext autoreload
+# %autoreload 2
+
+# # Chapter 16: Metric-Predicted Variable on One or Two Groups
---
+++ numpyro_glm/metric.py
@@ -0,0 +1,17 @@
+import jax.numpy as jnp
+import numpyro
+import numpyro.distributions as dist
+
+
+def no_predictors(y: jnp.array):
+    n, = jnp.shape(y)
+    data_mean = jnp.mean(y)
+    data_std = jnp.std(y)
+    
+    # Specify prior mean and standard deviation.
+    mean = numpyro.sample('mean', dist.Normal(data_mean, 100 * data_std))
+    std = numpyro.sample('std', dist.Uniform(data_std / 1000, data_std * 1000))
+
+    # Observations.
+    with numpyro.plate('obs', n) as idx:
+        numpyro.sample('y', dist.Normal(mean, std), obs=y[idx])
