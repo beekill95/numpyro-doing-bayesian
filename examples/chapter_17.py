8c9bc8353aab2f71fdaa6a302f50ac1cbbe27c8d
beekill95 <nguyenmbquan95@gmail.com>
2022-05-19 16:24:16 -0400

    Implement one metric predictor model for chapter 17.

--- examples/chapter_17.py
+++ examples/chapter_17.py
@@ -17,8 +17,15 @@
 # %load_ext autoreload
 # %autoreload 2
 
+import jax.numpy as jnp
+import jax.random as random
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import numpyro
+from numpyro.infer import MCMC, NUTS
+import numpyro_glm
+import numpyro_glm.metric.models as glm_metric
 from scipy.stats import norm
 
 # # Chapter 17: Metric Predicted Variable with one Metric Predictor
@@ -39,3 +46,37 @@ ax.plot(xline, 10 + 2 * xline, lw=4, c='#87ceeb')
 # TODO
 for xinterval in [-7.5, -2.5, 2.5, 7.5]:
     y_ = np.linspace(xinterval - 6, xinterval + 6, 1000)
+# -
+
+# ## Robust Linear Regression
+
+height_weight_30_data = pd.read_csv('datasets/HtWtData30.csv')
+height_weight_30_data.describe()
+
+# We will test the model with raw data (no standardization applied to both y and x).
+
+mcmc_key = random.PRNGKey(0)
+kernel = NUTS(glm_metric.one_metric_predictor_robust_no_standardization)
+mcmc = MCMC(kernel, num_warmup=1000, num_samples=20000)
+mcmc.run(
+    mcmc_key,
+    jnp.array(height_weight_30_data.height.values),
+    jnp.array(height_weight_30_data.weight.values),
+)
+mcmc.print_summary()
+
+numpyro_glm.plot_diagnostic(mcmc, ['b0', 'b1', 'nu', 'sigma'])
+
+# Here is the model with both y and x standardized.
+
+mcmc_key = random.PRNGKey(0)
+kernel = NUTS(glm_metric.one_metric_predictor_robust)
+mcmc = MCMC(kernel, num_warmup=1000, num_samples=20000)
+mcmc.run(
+    mcmc_key,
+    jnp.array(height_weight_30_data.height.values),
+    jnp.array(height_weight_30_data.weight.values),
+)
+mcmc.print_summary()
+
+numpyro_glm.plot_diagnostic(mcmc, ['zb0', 'zb1', 'nu', 'zsigma', 'b0'])
--- numpyro_glm/metric/models.py
+++ numpyro_glm/metric/models.py
@@ -51,3 +51,50 @@ def multi_groups_robust(y: jnp.ndarray, group: jnp.ndarray, nb_groups: int):
     with numpyro.plate('obs', n) as idx:
         numpyro.sample(
             'y', dist.StudentT(nu, mean[group[idx]], sigma[group[idx]]), obs=y[idx])
+
+
+def one_metric_predictor_robust_no_standardization(y: jnp.ndarray, x: jnp.ndarray):
+    assert jnp.shape(y)[0] == jnp.shape(x)[0]
+
+    n, = jnp.shape(y)
+
+    # Specify priors for b0, b1, sigma and normality parameter.
+    b0 = numpyro.sample('b0', dist.Normal(0, 10))
+    b1 = numpyro.sample('b1', dist.Normal(0, 10))
+    sigma = numpyro.sample('sigma', dist.Uniform(1e-3, 1e+3))
+    nu = numpyro.sample('nu', dist.Exponential(1. / 30))
+    mean = numpyro.deterministic('mean', b0 + x * b1)
+
+    # Observations.
+    with numpyro.plate('obs', n) as idx:
+        numpyro.sample('y', dist.StudentT(nu, mean[idx], sigma), obs=y[idx])
+
+
+def one_metric_predictor_robust(y: jnp.ndarray, x: jnp.ndarray):
+    """The same one metric predictor model but with standardization."""
+    assert jnp.shape(y)[0] == jnp.shape(x)[0]
+
+    n, = jnp.shape(y)
+
+    # Standardize the data.
+    mean_x, mean_y = (jnp.mean(val) for val in [x, y])
+    std_x, std_y = (jnp.std(val) for val in [x, y])
+    xz = (x - mean_x) / std_x
+    yz = (y - mean_y) / std_y
+
+    # Specify priors zb0, zb1, zsigma and normality paramter.
+    zb0 = numpyro.sample('zb0', dist.Normal(0, 10))
+    zb1 = numpyro.sample('zb1', dist.Normal(0, 10))
+    zsigma = numpyro.sample('zsigma', dist.Uniform(1e-3, 1e+3))
+    nu = numpyro.sample('nu', dist.Exponential(1. / 30))
+    zmean = numpyro.deterministic('zmean', zb0 + xz * zb1)
+
+    # Observations.
+    with numpyro.plate('zobs', n) as idx:
+        numpyro.sample('yz', dist.StudentT(nu, zmean, zsigma), obs=yz[idx])
+
+    # Transform back to the original scale.
+    numpyro.deterministic('b1', zb1 * std_y / std_x)
+    numpyro.deterministic(
+        'b0', zb0 * std_y + mean_y - zb1 * mean_x * std_y / std_x)
+    numpyro.deterministic('sigma', zsigma * std_y)
