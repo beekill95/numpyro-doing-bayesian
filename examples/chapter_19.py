723409438960ac29e487adc1b47987ef6eb9fc6d
beekill <nguyenmbquan95@gmail.com>
2022-06-15 07:16:22 +0700

    [Chapter 19] plot contrasts

--- examples/chapter_19.py
+++ examples/chapter_19.py
@@ -50,7 +50,10 @@ mcmc.run(
     len(fruit_df['CompanionNumber'].cat.categories))
 mcmc.print_summary()
 
-idata = az.from_numpyro(mcmc)
+idata = az.from_numpyro(
+    mcmc,
+    coords=dict(CompanionNumber=fruit_df['CompanionNumber'].cat.categories),
+    dims=dict(b_=['CompanionNumber']))
 az.plot_trace(idata)
 plt.tight_layout()
 
@@ -62,8 +65,8 @@ sns.swarmplot(fruit_df['CompanionNumber'], fruit_df['Longevity'], ax=ax)
 ax.set_xlim(xmin=-1)
 
 b0 = idata.posterior['b0'].values.flatten()
-b_ = {name: idata.posterior['b_'].sel(b__dim_0=i).values.flatten()
-      for i, name in enumerate(fruit_df['CompanionNumber'].cat.categories)}
+b_ = {name: idata.posterior['b_'].sel(CompanionNumber=name).values.flatten()
+      for name in fruit_df['CompanionNumber'].cat.categories}
 ySigma = idata.posterior['ySigma'].values.flatten()
 
 n_curves = 20
@@ -83,3 +86,69 @@ for i, name in enumerate(fruit_df['CompanionNumber'].cat.categories):
         ax.plot(-xrange + i, yrange)
 
 fig.tight_layout()
+
+
+# -
+
+# Plot contrasts to compare between groups.
+
+# +
+def plot_contrasts(idata: az.InferenceData, contrasts: 'list[dict]', figsize=(10, 6)):
+    fig, axes = plt.subplots(nrows=2, ncols=len(contrasts), figsize=figsize)
+    posterior = idata.posterior
+
+    def mean(values: list):
+        return sum(values) / len(values)
+
+    for i, contrast in enumerate(contrasts):
+        plot_title = f'{".".join(contrast["left"])}\nvs\n{".".join(contrast["right"])}'
+
+        # Plot difference.
+        ax = axes[0, i]
+        left = mean([posterior['b_'].sel(CompanionNumber=n).values
+                     for n in contrast['left']])
+        right = mean([posterior['b_'].sel(CompanionNumber=n).values
+                      for n in contrast['right']])
+        diff = left - right
+
+        az.plot_posterior(
+            diff, hdi_prob=0.95,
+            point_estimate='mode',
+            ref_val=contrast['refVal'], rope=contrast['rope'],
+            ax=ax)
+        ax.set_title(plot_title)
+        ax.set_xlabel('Difference')
+
+        # Plot effect size.
+        ax = axes[1, i]
+        effSize = diff / posterior['ySigma']
+
+        az.plot_posterior(
+            effSize, hdi_prob=0.95,
+            point_estimate='mode',
+            ref_val=contrast['effSizeRefVal'], rope=contrast['effSizeRope'],
+            ax=ax)
+        ax.set_title(plot_title)
+        ax.set_xlabel('Effect Size')
+
+    fig.tight_layout()
+    return fig
+
+
+contrasts = [
+    dict(left=['Pregnant1', 'Pregnant8'], right=['None0'],
+         refVal=0.0, rope=(-1.5, 1.5),
+         effSizeRefVal=0.0, effSizeRope=(-0.1, 0.1)),
+    dict(left=['Pregnant1', 'Pregnant8', 'None0'],
+         right=['Virgin1', 'Virgin8'],
+         refVal=0.0, rope=(-1.5, 1.5),
+         effSizeRefVal=0.0, effSizeRope=(-0.1, 0.1)),
+    dict(left=['Pregnant1', 'Pregnant8', 'None0'], right=['Virgin1'],
+         refVal=0.0, rope=(-1.5, 1.5),
+         effSizeRefVal=0.0, effSizeRope=(-0.1, 0.1)),
+    dict(left=['Virgin1'], right=['Virgin8'],
+         refVal=0.0, rope=(-1.5, 1.5),
+         effSizeRefVal=0.0, effSizeRope=(-0.1, 0.1)),
+]
+
+_ = plot_contrasts(idata, contrasts, figsize=(15, 6))
