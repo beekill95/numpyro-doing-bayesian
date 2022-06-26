745d16ce09e02522bf157b6c58ce94c8e3e55b62
beekill <nguyenmbquan95@gmail.com>
2022-06-26 11:45:54 +0700

    [Chapter 23] Plot all figures in one group model

--- notebooks/chapter_23.py
+++ notebooks/chapter_23.py
@@ -57,59 +57,70 @@ idata_yord_1 = az.from_numpyro(mcmc)
 az.plot_trace(idata_yord_1)
 plt.tight_layout()
 
+
 # +
-fig, axes = plt.subplots(nrows=3, ncols=2, figsize=(10, 6))
+def plot_ordinal_one_group_results(idata, df, thresholds):
+    fig, axes = plt.subplots(nrows=3, ncols=2, figsize=(12, 8))
 
-# Plot latent score's mean posterior distribution.
-ax = axes[0, 0]
-az.plot_posterior(
-    idata_yord_1,
-    var_names='mu',
-    hdi_prob=0.95,
-    ref_val=2,
-    point_estimate='mode',
-    ax=ax)
-ax.set_title('Mean')
-ax.set_xlabel('$\mu$')
+    # Plot latent score's mean posterior distribution.
+    ax = axes[0, 0]
+    az.plot_posterior(
+        idata,
+        var_names='mu',
+        hdi_prob=0.95,
+        ref_val=2,
+        point_estimate='mode',
+        ax=ax)
+    ax.set_title('Mean')
+    ax.set_xlabel('$\mu$')
 
-# Plot data with posterior distribution.
-ax = axes[0, 1]
-ax.hist(ord_1_df['Y'], bins=yord_1_cat.categories.size)
+    # Plot data with posterior distribution.
+    ax = axes[0, 1]
+    ordinal_plots.plot_ordinal_data_with_posterior(
+        idata, 'mu', 'sigma', thresholds,
+        df, ordinal_predicted='Y',
+        ax=ax)
+    ax.set_title('Data with Post. Pred. Distrib.')
 
-# Plot latent score's standard deviation.
-ax = axes[1, 0]
-az.plot_posterior(
-    idata_yord_1,
-    var_names='sigma',
-    hdi_prob=0.95,
-    point_estimate='mode',
-    ax=ax)
-ax.set_title('Std.')
-ax.set_xlabel('$\sigma$')
+    # Plot latent score's standard deviation.
+    ax = axes[1, 0]
+    az.plot_posterior(
+        idata,
+        var_names='sigma',
+        hdi_prob=0.95,
+        point_estimate='mode',
+        ax=ax)
+    ax.set_title('Std.')
+    ax.set_xlabel('$\sigma$')
 
-# Plot effect size.
-ax = axes[1, 1]
-posterior = idata_yord_1.posterior
-eff_size = (posterior['mu'] - 2) / posterior['sigma']
-az.plot_posterior(
-    eff_size,
-    hdi_prob=0.95,
-    point_estimate='mode',
-    ref_val=0,
-    ax=ax)
-ax.set_title('Effect Size')
-ax.set_xlabel('$(\mu - 2) / \sigma$')
+    # Plot effect size.
+    ax = axes[1, 1]
+    posterior = idata.posterior
+    eff_size = (posterior['mu'] - 2) / posterior['sigma']
+    az.plot_posterior(
+        eff_size,
+        hdi_prob=0.95,
+        point_estimate='mode',
+        ref_val=0,
+        ax=ax)
+    ax.set_title('Effect Size')
+    ax.set_xlabel('$(\mu - 2) / \sigma$')
 
-# Plot thresholds.
-ax = axes[2, 0]
-ordinal_plots.plot_threshold_scatter(
-    idata_yord_1,
-    ['thres_1', 'thres_2', 'thres_3', 'thres_4', 'thres_5', 'thres_6'],
-    ax=ax)
+    # Plot thresholds.
+    ax = axes[2, 0]
+    ordinal_plots.plot_threshold_scatter(idata, thresholds, ax=ax)
 
-axes[2, 1].remove()
-fig.tight_layout()
+    axes[2, 1].remove()
+    fig.tight_layout()
+
+
+thresholds = ['thres_1', 'thres_2', 'thres_3', 'thres_4', 'thres_5', 'thres_6']
+plot_ordinal_one_group_results(idata_yord_1, ord_1_df, thresholds)
 # -
+
+# ---
+#
+# The same model but applied on different dataset.
 
 ord_2_df = pd.read_csv('datasets/OrdinalProbitData-1grp-2.csv')
 yord_2_cat = pd.CategoricalDtype([1, 2, 3, 4, 5, 6, 7], ordered=True)
@@ -129,3 +140,5 @@ mcmc.print_summary()
 idata_yord_2 = az.from_numpyro(mcmc)
 az.plot_trace(idata_yord_2)
 plt.tight_layout()
+
+plot_ordinal_one_group_results(idata_yord_2, ord_2_df, thresholds)
--- numpyro_glm/ordinal/plots.py
+++ numpyro_glm/ordinal/plots.py
@@ -1,7 +1,9 @@
 import arviz as az
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import seaborn as sns
+from scipy.stats import norm
 
 
 def plot_threshold_scatter(idata: az.InferenceData, thresholds: 'list[str]', ax: plt.Axes = None):
@@ -14,7 +16,7 @@ def plot_threshold_scatter(idata: az.InferenceData, thresholds: 'list[str]', ax:
         Inference Data returned from numpyro.
     thresholds: list[str]
         Name of the thresholds to be plotted.
-        Assume that the first and the last are deterministic variable.
+        Assume that the first and the last are deterministic variables.
     ax: plt.Axes
         Matplotlib's axes for plotting.
     """
@@ -29,5 +31,44 @@ def plot_threshold_scatter(idata: az.InferenceData, thresholds: 'list[str]', ax:
 
     for vals in values:
         sns.scatterplot(x=vals, y=means, ax=ax)
+
+    return ax
+
+
+def plot_ordinal_data_with_posterior(
+        idata: az.InferenceData, latent_mean: str, latent_sd: str, thresholds: 'list[str]',
+        data: pd.DataFrame, ordinal_predicted='Y',
+        ax: plt.Axes = None):
+    if ax is None:
+        _, ax = plt.subplots()
+
+    # Plot data.
+    sns.histplot(
+        data, x=ordinal_predicted,
+        bins=data[ordinal_predicted].cat.categories,
+        stat='proportion',
+        ax=ax)
+
+    # Plot posterior distribution.
+    posterior = idata['posterior']
+    mean = posterior[latent_mean].values.flatten()
+    sd = posterior[latent_sd].values.flatten()
+    thres = np.asarray([posterior[f'{t}'].values.flatten()
+                        for t in thresholds])
+    cdf = norm.cdf((thres - mean[None, :]) / sd[None, :])
+    probs = np.zeros((len(thresholds) + 1, mean.size), dtype=np.float32)
+    probs[0] = cdf[0]
+    probs[-1] = 1. - cdf[-1]
+    probs[1:-1] = cdf[1:] - cdf[:-1]
+
+    probs_mode = np.median(probs, axis=1)
+    probs_95_hdi = az.hdi(probs.T - probs_mode[None, :], hdi_prob=.95)
+
+    ax.errorbar(
+        x=data[ordinal_predicted].cat.categories,
+        y=probs_mode,
+        yerr=np.abs(probs_95_hdi.T),
+        fmt='o',
+        elinewidth=3.)
 
     return ax
