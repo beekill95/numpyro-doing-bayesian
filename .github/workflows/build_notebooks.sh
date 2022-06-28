b284377b2439d70ebedada6e3a22d99ebd054ee1
beekill <nguyenmbquan95@gmail.com>
2022-06-28 16:30:03 +0700

    Fix build_notebooks.sh

--- .github/workflows/build_notebooks.sh
+++ .github/workflows/build_notebooks.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-NOTEBOOKS_PY="notebooks/*.py"
+NOTEBOOKS_PY=(notebooks/*.py)
 OUTPUT_DIR="outputs"
 
 # Create output directory if necessary.
 mkdir -p "$OUTPUT_DIR"
 
-for file in "$NOTEBOOKS_PY"; do
+for file in "${NOTEBOOKS_PY[@]}"; do
     filename=$(basename "$file")
-    echo "Converting and Executing $file to notebook."
-    jupytext --to notebook -o "$OUTPUT_DIR/${filename%.*}" --execute "$file"
+    echo "Converting and executing $file."
+    jupytext --to notebook -o "$OUTPUT_DIR/${filename%.*}.ipynb" --execute "$file"
 done
