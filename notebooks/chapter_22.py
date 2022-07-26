28639eda7f7f8c6ee420bba6b7b74a2e5cd16ec7
beekill <nguyenmbquan95@gmail.com>
2022-07-26 10:42:30 +0700

    Add notebooks' title

--- notebooks/chapter_22.py
+++ notebooks/chapter_22.py
@@ -2,6 +2,7 @@
 # jupyter:
 #   jupytext:
 #     formats: ipynb,py:light
+#     notebook_metadata_filter: title, author
 #     text_representation:
 #       extension: .py
 #       format_name: light
@@ -11,6 +12,7 @@
 #     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
+#   title: '[Doing Bayesian Data Analysis] Chapter 22: Nominal Predicted Variable'
 # ---
 
 # %cd ..
@@ -25,12 +27,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import numpyro
 from numpyro.infer import MCMC, NUTS
-from numpyro.infer.initialization import init_to_feasible, init_to_median
-import numpyro_glm
+from numpyro.infer.initialization import init_to_median
 import numpyro_glm.logistic.models as glm_logistic
 import pandas as pd
 from scipy.special import expit
-from scipy.stats import beta
 import seaborn as sns
 
 numpyro.set_host_device_count(4)
