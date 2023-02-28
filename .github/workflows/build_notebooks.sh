baf1da1ea835954f910f9e2d047517b1436f69b6
beekill95 <nguyenmbquan95@gmail.com>
2023-02-28 11:39:31 -0500

    Use doit to publish notebooks.

--- .github/workflows/build_notebooks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NOTEBOOKS_PY=(notebooks/*.py)
-
-for file in "${NOTEBOOKS_PY[@]}"; do
-    filename=$(basename "$file")
-    echo "Converting and executing $file."
-    jupytext --to notebook --execute "$file"
-done
