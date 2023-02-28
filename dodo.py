7b7117086aae86d994786c8193d7a25c1cf2d5e4
beekill95 <nguyenmbquan95@gmail.com>
2023-02-28 10:22:20 -0500

    Implement tasks to publish from .py files to html files.

--- dodo.py
+++ dodo.py
@@ -3,13 +3,47 @@ import glob
 import numpyro_glm
 import os
 
+OUTPUT_DIR = 'docs'
+PY_FILES = glob.glob('notebooks/chapter_*.py')
 
-def task_to_notebook():
-    py_files = glob.iglob('notebooks/chapter_*.py')
-    return (dict(
-        file_dep=[file],
-        name=os.path.basename(file),
-        actions=['echo TODO'],
-        uptodate=[config_changed(numpyro_glm.__version__)],
-        verbosity=2,
-    ) for file in py_files)
+
+def notebook_output_path(py_file: str):
+    filename = os.path.basename(py_file)
+    filename, _ = os.path.splitext(filename)
+    dir = os.path.dirname(py_file)
+    return os.path.join(dir, f'{filename}.ipynb')
+
+
+def html_output_path(output_dir: str, py_file: str):
+    filename = os.path.basename(py_file)
+    filename, _ = os.path.splitext(filename)
+    return os.path.join(output_dir, f'{filename}.html')
+
+
+def task_notebook():
+    def task_spec(path: str):
+        return dict(
+            file_dep=[path],
+            name=os.path.basename(path),
+            actions=[f'jupytext --to notebook --execute "{path}"'],
+            targets=[notebook_output_path(path)],
+            uptodate=[config_changed(numpyro_glm.__version__)],
+            verbosity=2,
+        )
+
+    return (task_spec(file) for file in PY_FILES)
+
+
+def task_publish():
+    def task_spec(path: str):
+        notebook = notebook_output_path(path)
+        return dict(
+            name=os.path.basename(path),
+            file_dep=[notebook, './html_template/index.html.j2'],
+            actions=[
+                f'jupyter nbconvert --to html --embed-images --no-prompt --template ./html_template --output-dir="{OUTPUT_DIR}" "{notebook}"'],
+            targets=[html_output_path(OUTPUT_DIR, path)],
+            verbosity=2,
+        )
+
+    return (task_spec(file) for file in PY_FILES)
